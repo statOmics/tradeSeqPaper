9c32bc55f062f44428b3d11bf2c64add1662d7f6
Hector <hector.rouxdebezieux@berkeley.edu>
2019-07-30 15:00:09 +0200

    better code coherence

--- simulation/time/generate_dyntoy_dataset.R
+++ simulation/time/generate_dyntoy_dataset.R
@@ -2,12 +2,13 @@ library(tidyverse)
 library(dyno)
 library(dyntoy)
 library(patchwork)
+library(here)
 
 
-  dataset <- generate_dataset(
-    model = model_bifurcating(),
-    num_cells = 10000,
-    num_features = 5000,
-    differentially_expressed_rate = .2
-  )
-  saveRDS(dataset, file=paste0("~/Dropbox/PhD/Research/singleCell/trajectoryInference/trajectoryDE/tradeSeqPaper/simulation/time/bigDyntoyDataset.rds"))
+dataset <- generate_dataset(
+  model = model_bifurcating(),
+  num_cells = 10000,
+  num_features = 5000,
+  differentially_expressed_rate = .2
+)
+saveRDS(dataset, file = paste0(here("simulation", "time", "bigDyntoyDataset.rds")))
