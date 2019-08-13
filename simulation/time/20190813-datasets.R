cf6c48d335c5a9b7d2030b9524d855f71881678a
Hector <hector.rouxdebezieux@berkeley.edu>
2019-08-13 10:35:38 +0200

    datasets created ind

---
+++ simulation/time/20190813-datasets.R
@@ -0,0 +1,27 @@
+library(here)
+library(dyno)
+library(dyntoy)
+library(tidyverse)
+library(wesanderson)
+
+## Pre-process ----
+NCORES <- 2
+palette(wes_palette("Darjeeling1", 10, type = "continuous"))
+source(here::here("simulation", "time", "20190611_helper.R"))
+set.seed(87657)
+
+## datasets ----
+
+for (size in 4:5) {
+  ## Generate dataset ----
+  print("dataset")
+  dataset <- generate_dataset(
+    model = model_bifurcating(),
+    num_cells = 10^size,
+    num_features = 5000,
+    differentially_expressed_rate = .2
+  )
+  saveRDS(dataset, here::here("simulation", "time",
+                              paste0(size, "dataset.rds")))
+  
+}
---
+++ simulation/time/getEstimates copy.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#SBATCH --mail-user=hector.rouxdebezieux@berkeley.edu
+#SBATCH --mail-type=ALL
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1
+#SBATCH --nodes=1
+
+R CMD BATCH --no-save 20190813-datasets.R 20190813-datasets.Rout
