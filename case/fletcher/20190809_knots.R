f37d7d3ec03bc3ee941e185fc0db079c485a3fe6
koenvandenberge <koen.vdberge@gmail.com>
2019-08-16 13:14:50 +0200

    colors OE trajectory knots

--- case/fletcher/20190809_knots.R
+++ case/fletcher/20190809_knots.R
@@ -39,6 +39,8 @@ pseudotime[closestCell3,3] #OK
 
 cols <- c("#FF7F00", "#1F78B4", "#E7298A")
 
+colpal <- c("#1B9E77","antiquewhite2","cyan","#E7298A","#A6CEE3","#666666","#E6AB02","#FFED6F","darkorchid2","#B3DE69","#FF7F00","#A6761D","#1F78B4")
+
 # 3D plot
 rgl::plot3d(X[, 1:3], t = "p", col = colpal[as.factor(clus.labels)], alpha = 0.3, pch = 19, cex = 2, size = 8, xlab = "PC 1", ylab = "PC 2", zlab = "PC 3", aspect = "iso", box = FALSE, axes = FALSE)
 # rgl::plot3d(X[,1:3], t='p', col=c("white","black")[hlp+1],alpha=0.3, pch = 19, cex = 2, size=8, xlab="PC 1", ylab="PC 2", zlab="PC 3", aspect="iso", box=FALSE, axes=FALSE)
@@ -48,9 +50,9 @@ for (i in seq_along(curves)) {
   rgl::plot3d(crv@curves[[i]]$s[order(crv@curves[[i]]$lambda), 1:3], type = "l", add = TRUE, lwd = 4, col = colpal[which.max(tail(lin@lineages[[i]], 1) == levels(clus.labels))])
 }
 #rgl::pch3d(crv@curves[[1]]$s[closestCell,1:3], pch=2, add=TRUE)
-rgl::pch3d(crv@curves[[1]]$s[closestCell,1:3], pch=as.character(1:6), add=TRUE)
+rgl::pch3d(crv@curves[[1]]$s[closestCell,1:3], pch=as.character(1:6), add=TRUE, cex=3/2)
 #rgl::pch3d(crv@curves[[2]]$s[closestCell2[-1],1:3], pch=2, add=TRUE)
-rgl::pch3d(crv@curves[[2]]$s[closestCell2[-1],1:3], pch=as.character(2:5), add=TRUE)
+rgl::pch3d(crv@curves[[2]]$s[closestCell2[-1],1:3], pch=as.character(2:5), add=TRUE, cex=3/2)
 #rgl::pch3d(crv@curves[[3]]$s[closestCell3[-1],1:3], pch=2, add=TRUE, size=9)
-rgl::pch3d(crv@curves[[3]]$s[closestCell3[-1],1:3], pch=as.character(2:4), add=TRUE, size=9)
+rgl::pch3d(crv@curves[[3]]$s[closestCell3[-1],1:3], pch=as.character(2:4), add=TRUE, cex=3/2)
 rgl.postscript("~/fletcher3d_knots.pdf", fmt="pdf")
