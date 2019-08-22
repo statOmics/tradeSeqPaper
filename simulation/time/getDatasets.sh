c85a91d971c649a4956ef4dc959baf992e32c951
Hector Roux de Bezieux <hector.rouxdebezieux@arwen.Berkeley.EDU>
2019-08-22 16:23:20 -0700

    running tradeSeq with fewer knots

--- simulation/time/getDatasets.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#SBATCH --mail-user=hector.rouxdebezieux@berkeley.edu
-#SBATCH --mail-type=ALL
-#SBATCH --ntasks=1
-#SBATCH --cpus-per-task=1
-#SBATCH --nodes=1
-
-R CMD BATCH --no-save 20190813-datasets.R 20190813-datasets.Rout
